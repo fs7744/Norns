4cf59ec571de818f54e0e18664ce62136c7d18f0
Victor.X.Qu <fs7744@hotmail.com>
2018-11-15 20:14:59 +0800

    add

---
+++ norns/src/Norns.StaticWeave.MSBuild/WeavingTask.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+using System;
+
+namespace Norns.StaticWeave.MSBuild
+{
+    public class WeavingTask : Task
+    {
+        [Required]
+        public string MSBuildProjectDirectory { get; set; }
+
+        public override bool Execute()
+        {
+            Log.LogMessage(MessageImportance.High, $"Hello {MSBuildProjectDirectory}");
+            return true;
+        }
+    }
+}
